a7192d9afafb36bdf3e608dbbaab22f0c17dc749
Joseph Jackson <96963864+jwseph@users.noreply.github.com>
2022-11-14 11:56:39 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+// jQuery
+let jqScript = document.createElement('script');
+jqScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+document.getElementsByTagName('head')[0].appendChild(jqScript);
+*/
+
+let canvas = $('#rso > div:nth-child(1) > div > block-component > div > div.dG2XIf.Wnoohf.OJXvsb > div > div > div > div.ifM9O > div > div > div > div > div:nth-child(2) > g-lightbox > div > div.ynlwjd.VDgVie.qzMpzb.u98ib > div.AU64fe.zsYMMe.TUOsUe > span > div > canvas');
+let difficulty = $('#ow36 > div.rIbAWc').textContent;
+
+// unfinished
